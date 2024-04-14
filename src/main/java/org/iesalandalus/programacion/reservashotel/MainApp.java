bc99bb5bcf54b4e9c1dde4dae612aa9799ffc6dd
Jose Javier Sierra Berdun <jsieber1908@gmail.com>
2024-04-14 21:25:38 +0200

    MainApp

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -2,6 +2,7 @@ package org.iesalandalus.programacion.reservashotel;
 
 
 import org.iesalandalus.programacion.reservashotel.controlador.Controlador;
+import org.iesalandalus.programacion.reservashotel.modelo.FactoriaFuenteDatos;
 import org.iesalandalus.programacion.reservashotel.modelo.Modelo;
 import org.iesalandalus.programacion.reservashotel.modelo.dominio.Regimen;
 import org.iesalandalus.programacion.reservashotel.modelo.dominio.TipoHabitacion;
@@ -11,10 +12,19 @@ public class MainApp {
     public static void main(String[] args) {
 
         Vista vista = new Vista();
-        Modelo modelo = new Modelo();
+        Modelo modelo = procesarArgumentosFuenteDatos(args[0]);
         Controlador controlador = new Controlador(modelo, vista);
-
         controlador.comenzar();
     }
 
+    private static Modelo procesarArgumentosFuenteDatos(String args){
+        Modelo modelo = new Modelo(FactoriaFuenteDatos.MEMORIA);
+        if (args.equals("-fdmemoria")){
+            modelo = new Modelo(FactoriaFuenteDatos.MEMORIA);
+        }
+        if (args.equals("-fdmongodb")){
+            modelo = new Modelo(FactoriaFuenteDatos.MONGODB);
+        }
+        return modelo;
+    }
 }
